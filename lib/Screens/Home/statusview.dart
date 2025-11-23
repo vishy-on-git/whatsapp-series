d07449cd9e0e4e22af49bb89d35c267afb5a5a81
ARYAN VISHWAKARMA <aryanv9361@gmail.com>
2025-11-23 16:40:20 +0530

    Completed Status Screen

--- lib/Screens/Home/statusview.dart
+++ lib/Screens/Home/statusview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:whatsapp_series/Screens/Home/Data/statusmodel.dart';
 import 'package:whatsapp_series/Widgets/uihelper.dart';
 
@@ -8,16 +9,83 @@ class Statusview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: statuses.length,
+      itemCount: statuses.length + 2,
       itemBuilder: (BuildContext context, int index) {
-        return StatusTile(status: statuses[index]);
+        if (index == 0) {
+          return MyStatusTile();
+        }
+        if(index==1){
+          return Padding(padding: EdgeInsets.only(top: 6,bottom: 6,left: 30),child: UiHelper.CustomText(text: "Recent Updates", height: 14,color: Color(0xFF758289)),);
+        }
+        return StatusTile(status: statuses[index - 2]);
       },
     );
   }
 }
 
+class MyStatusTile extends StatelessWidget {
+  const MyStatusTile({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+      height: 78,
+      child: Row(
+        spacing: 12,
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 66,
+                width: 66,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      "/Users/vishyaryan/StudioProjects/whatsapp_series/assets/images/black-silhouette-of-faceless-person-with-curly-hair-for-artistic-design-projects-vector.jpg",
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 1,
+                bottom: 1,
+                child: Container(
+                  height: 20,
+                  width: 20,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Container(
+                    height: 12,
+                    width: 12,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFF00A884),
+                    ),
+                    child: Icon(Icons.add, color: Colors.white, size: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(child: Column(
+            spacing: 3.0,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              UiHelper.CustomText(text: "MyStatus", height: 15,color: Color(0xFF3A4B55),fontweight: FontWeight.bold),
+              UiHelper.CustomText(text: "Tap to add status update", height: 11,color: Color(0xFF758289))
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
 
 class StatusTile extends StatelessWidget {
   final Statusmodel status;
@@ -26,12 +94,13 @@ class StatusTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String statusTime = DateFormat.jm().format(status.timeAgo);
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+      padding: EdgeInsets.symmetric(vertical: 7, horizontal: 7),
       height: 78,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
-        spacing: 8.0,
+        spacing: 0.1,
         children: [
           Container(
             padding: EdgeInsets.all(3),
@@ -49,7 +118,7 @@ class StatusTile extends StatelessWidget {
               padding: EdgeInsets.all(2),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.white
+                color: Colors.white,
               ),
               child: CircleAvatar(
                 radius: 40,
@@ -58,16 +127,27 @@ class StatusTile extends StatelessWidget {
               ),
             ),
           ),
-          
-          Expanded(child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 4,
-            children: [
-              UiHelper.CustomText(text: statuses[0].name, height: 16,fontweight: FontWeight.bold, color: Colors.black),
-              UiHelper.CustomText(text: status.timeAgo.toString(), height: 10,color: Color(0xFF3A4B55))
-            ],
-          ))
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 4,
+              children: [
+                UiHelper.CustomText(
+                  text: status.name,
+                  height: 16,
+                  fontweight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                UiHelper.CustomText(
+                  text: statusTime,
+                  height: 10,
+                  color: Color(0xFF3A4B55),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
